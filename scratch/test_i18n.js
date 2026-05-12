96280a47267c2106237da4fc3b3d840f049487f2
Sayak Das <sayakdas.slsn8cd@gmail.com>
2026-05-13 02:38:24 +0530

    chore: cleaned repository structure and improved project presentation

--- scratch/test_i18n.js
+++
@@ -1,5 +0,0 @@
-
-const { t, translations } = require('./frontend/src/utils/i18n.js');
-console.log('Testing bn videoHeroTitle1:', t('videoHeroTitle1', 'bn'));
-console.log('Testing bn videoVerify:', t('videoVerify', 'bn'));
-console.log('Testing en videoHeroTitle1:', t('videoHeroTitle1', 'en'));
