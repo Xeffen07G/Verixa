e5856efedb1257428ce2aefc20984b1ef1fcdf1b
Sayak <sayakdas.slsn8cd@gmail.com>
2026-05-13 23:53:16 +0530

    feat: synchronize build indicator with latest commit 0e763fe

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-window.__VERIXA_BUILD__ = "feecf05";
+window.__VERIXA_BUILD__ = "0e763fe";
 import ReactDOM from 'react-dom/client';
 import './styles/global.css';
 import App from './App';
